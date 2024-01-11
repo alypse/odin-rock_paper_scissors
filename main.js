c396b03f9fa2795341685a138ed943b47eb7fb8d
alypse <44419772+alypse@users.noreply.github.com>
2024-01-11 12:37:21 +0000

    Remove match loop in preparation for refactor

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ const game = () => {
 
     document.querySelectorAll('button').forEach(function (button) {
         button.addEventListener('click', function () {
-            playRound(button.id,getComputerChoice());
+        playRound(button.id, getComputerChoice());
         })
-    });
+    })
 
     const getComputerChoice = () => {
         const choices = ["Rock", "Paper", "Scissors"];
@@ -24,32 +24,26 @@ const game = () => {
         getComputerChoice();
 
         if (playerSelection === computerSelection) {
-            playerHand.src=`/assets/${playerSelection}.png`;
-            computerHand.src=`/assets/${computerSelection}.png`;
-            roundResult.textContent =`Tie! You and the computer both chose ${playerSelection}!`;
-        }
-
-        else if ((playerSelection === "Rock" && computerSelection === "Scissors") ||
+            playerHand.src = `/assets/${playerSelection}.png`;
+            computerHand.src = `/assets/${computerSelection}.png`;
+            roundResult.textContent = `Tie! You and the computer both chose ${playerSelection}!`;
+        } else if ((playerSelection === "Rock" && computerSelection === "Scissors") ||
             (playerSelection === "Scissors" && computerSelection === "Paper") ||
             (playerSelection === "Paper" && computerSelection === "Rock")) {
             pScore++;
             playerScoreboard.textContent = `${pScore}`;
-            playerHand.src=`/assets/${playerSelection}.png`;
-            computerHand.src=`/assets/${computerSelection}.png`;
+            playerHand.src = `/assets/${playerSelection}.png`;
+            computerHand.src = `/assets/${computerSelection}.png`;
             roundResult.textContent = `You Win! You chose ${playerSelection} and the computer chose ${computerSelection}`;
-        }
-
-        else if ((playerSelection === "Rock" && computerSelection === "Paper") ||
+        } else if ((playerSelection === "Rock" && computerSelection === "Paper") ||
             (playerSelection === "Scissors" && computerSelection === "Rock") ||
             (playerSelection === "Paper" && computerSelection === "Scissors")) {
             cScore++;
             computerScoreboard.textContent = `${cScore}`;
-            playerHand.src=`/assets/${playerSelection}.png`;
-            computerHand.src=`/assets/${computerSelection}.png`;
+            playerHand.src = `/assets/${playerSelection}.png`;
+            computerHand.src = `/assets/${computerSelection}.png`;
             roundResult.textContent = `You Lose! You chose ${playerSelection} and the computer chose ${computerSelection}`;
-        }
-        // Provide logging for arguments outside of the scope of the if statements. ie; case, types, etc.
-        else roundResult.textContent = `No match, something has gone terribly wrong! You: ${playerSelection}, Computer: ${computerSelection}`;
+        } else roundResult.textContent = `Error! You: ${playerSelection}, Computer: ${computerSelection}`;
     }
 }
 
@@ -62,25 +56,8 @@ game();
 
 
 
-// Logic for a match
-// for (let rounds = 0; (pScore < 3 && cScore < 3); rounds++) {
-//     // let playerSelection = playerChoice
-//     let computerSelection = getComputerChoice()
-//     console.log(playRound(playerSelection, computerSelection)); // Log the return of playRound
-//
-//     if (pScore === 3) {
-//         console.log("You win, groovy!");
-//         break;
-//     }
-//     if (cScore === 3) {
-//         console.log("Computer wins, you suck!");
-//         break;
-//     }
-//     if (rounds === 20) {
-//         console.log("You've been playing for 20 rounds, let's call this a tie..")
-//         break;
-//     }
-// }
+
+
 
 
 
