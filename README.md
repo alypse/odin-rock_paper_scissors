a33ef1ef27689563c1c98b55b57426eafba1e2f1
alypse <44419772+alypse@users.noreply.github.com>
2024-01-12 14:59:00 +0000

    Functioning match with best of five

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Odin - Rock, Paper, Scissors
 
 Rock, paper, scissors game project for The Odin Project - Foundations course.
+
+
+## The project depended on some basic skills;
+- DOM manipulation
+- Functions
+- HTML and CSS basics
+
+
+## Potential iterations for later;
+- A 'start game' method which initialises the match interface
+- A distinct 'game over' view which has more explicit visual cues for the match outcome, and a method to restart the match
+- A general cleanup and visual improvement effort
+- Update visual assets...or an option to define the game (winning score, option names, image uploads for options!)
--- main.js
+++ main.js
@@ -2,49 +2,75 @@ const game = () => {
     let pScore = 0;
     let cScore = 0;
 
+    // Add event listener to each button and initialise a round
     document.querySelectorAll('button').forEach(function (button) {
         button.addEventListener('click', function () {
-        playRound(button.id, getComputerChoice());
+            playRound(button.id, getComputerChoice());
         })
     })
 
-    const getComputerChoice = () => {
-        const choices = ["Rock", "Paper", "Scissors"];
-        const randomIndex = Math.floor(Math.random() * (choices.length));
-        return choices[randomIndex];
-    }
-
-    function playRound(playerSelection, computerSelection) {
+    // Manage the match state
+    function matchTracker(playOutcome,playerSelection,computerSelection) {
         const roundResult = document.querySelector(".resultsView");
         const playerHand = document.querySelector(".playerChoice");
         const computerHand = document.querySelector(".computerChoice");
         const playerScoreboard = document.querySelector(".playerScore");
         const computerScoreboard = document.querySelector(".computerScore");
         const gameStatus = document.querySelector(".selectionsView");
-        getComputerChoice();
 
-        if (playerSelection === computerSelection) {
+        if (playOutcome === "tie") {
             playerHand.src = `/assets/${playerSelection}.png`;
             computerHand.src = `/assets/${computerSelection}.png`;
             roundResult.textContent = `Tie! You and the computer both chose ${playerSelection}!`;
-        } else if ((playerSelection === "Rock" && computerSelection === "Scissors") ||
-            (playerSelection === "Scissors" && computerSelection === "Paper") ||
-            (playerSelection === "Paper" && computerSelection === "Rock")) {
-            pScore++;
-            playerScoreboard.textContent = `${pScore}`;
-            playerHand.src = `/assets/${playerSelection}.png`;
-            computerHand.src = `/assets/${computerSelection}.png`;
-            roundResult.textContent = `You Win! You chose ${playerSelection} and the computer chose ${computerSelection}`;
-        } else if ((playerSelection === "Rock" && computerSelection === "Paper") ||
-            (playerSelection === "Scissors" && computerSelection === "Rock") ||
-            (playerSelection === "Paper" && computerSelection === "Scissors")) {
-            cScore++;
+        }
+        if (playOutcome === "computer_win") {
             computerScoreboard.textContent = `${cScore}`;
             playerHand.src = `/assets/${playerSelection}.png`;
             computerHand.src = `/assets/${computerSelection}.png`;
             roundResult.textContent = `You Lose! You chose ${playerSelection} and the computer chose ${computerSelection}`;
-        } else roundResult.textContent = `Error! You: ${playerSelection}, Computer: ${computerSelection}`;
+            if (cScore >= 5) {
+                gameStatus.textContent = "Computer wins the game!"
+            }
+        }
+        if (playOutcome === "player_win") {
+            playerScoreboard.textContent = `${pScore}`;
+            playerHand.src = `/assets/${playerSelection}.png`;
+            computerHand.src = `/assets/${computerSelection}.png`;
+            roundResult.textContent = `You Win! You chose ${playerSelection} and the computer chose ${computerSelection}`;
+            if (pScore >= 5) {
+                gameStatus.textContent = "You win the game!"
+            }
+        }
+        if (playOutcome === "error") {
+            roundResult.textContent = `Error! You: ${playerSelection}, Computer: ${computerSelection}`;
+        }
     }
+
+    // Get a random selection for the computer's hand
+    const getComputerChoice = () => {
+        const choices = ["Rock", "Paper", "Scissors"];
+        const randomIndex = Math.floor(Math.random() * (choices.length));
+        return choices[randomIndex];
+    }
+
+    // Play a single round and determine the winner. Pass the outcome to the match function
+    function playRound(playerSelection, computerSelection) {
+        getComputerChoice();
+
+            if (playerSelection === computerSelection) {
+                matchTracker("tie",playerSelection,computerSelection);
+            } else if ((playerSelection === "Rock" && computerSelection === "Scissors") ||
+                (playerSelection === "Scissors" && computerSelection === "Paper") ||
+                (playerSelection === "Paper" && computerSelection === "Rock")) {
+                pScore++;
+                matchTracker("player_win",playerSelection,computerSelection);
+            } else if ((playerSelection === "Rock" && computerSelection === "Paper") ||
+                (playerSelection === "Scissors" && computerSelection === "Rock") ||
+                (playerSelection === "Paper" && computerSelection === "Scissors")) {
+                cScore++;
+                matchTracker("computer_win",playerSelection,computerSelection);
+            } else matchTracker("error",playerSelection,computerSelection);
+        }
 }
 
 game();
